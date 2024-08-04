a58f01d56686a2a443813444963e61448236746e
maukan <maukanm22@gmail.com>
2024-08-04 13:51:07 -0400

    fixed bug

---
+++ packages/queries.sql
@@ -0,0 +1,12 @@
+CREATE TABLE salaries_and_stats AS
+SELECT 
+    salaries.salary,
+    stats.*
+FROM 
+    master_player_salaries AS salaries
+INNER JOIN 
+    player_stats AS stats 
+ON 
+    stats."Name" = salaries.name
+AND 
+    stats.season = salaries.season;
